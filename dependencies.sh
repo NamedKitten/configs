31f160402143a94d7f0193356897a9570798b887
Tim Wanders <timwanders241@gmail.com>
2017-05-10 20:12:43 +0200

    Added dep install script

---
+++ dependencies.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+echo "This will require multilib and the french arch repo added to your pacman.conf!"
+sudo pacman -S gparted xscreensaver pidgin steam chromium tint2 geany xorg-server xorg-xinit thunar xorg-xrandr xterm openbox lxterminal yaourt lxde lxappearance arandr nitrogen gksu vlc tint2 mate-system-monitor 
+yaourt -S adapta-gtk-theme gnome-alsamixer openbox-menu volumeicon galculator
