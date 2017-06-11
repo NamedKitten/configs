d756df44f97032c529b1a416f73df1a0f431960d
Tim Wanders <timwanders241@gmail.com>
2017-06-11 18:43:35 +0200

    Added xfce4-screenshooter

--- dependencies.sh
+++ dependencies.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 echo "This will require multilib and the french arch repo added to your pacman.conf!"
-sudo pacman -S gparted xscreensaver pidgin steam chromium tint2 geany xorg-server xorg-xinit thunar xorg-xrandr xterm openbox lxterminal yaourt lxde lxappearance arandr nitrogen gksu vlc tint2 mate-system-monitor redshift hexchat pidgin-otr firefox aspell-en qt4
+sudo pacman -S gparted xscreensaver pidgin steam chromium tint2 geany xorg-server xorg-xinit thunar xorg-xrandr xterm openbox lxterminal yaourt lxde lxappearance arandr nitrogen gksu vlc tint2 mate-system-monitor redshift hexchat pidgin-otr firefox aspell-en qt4 xfce4-screenshooter
 yaourt -S gmrun adapta-gtk-theme gnome-alsamixer openbox-menu volumeicon galculator rofi compton plank
 git clone --depth=1 https://github.com/Bash-it/bash-it.git ~/.bash_it
 sh ~/.bash_it/install.sh
