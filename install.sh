e5d0afeca818811d665271986b52c80eb7ec1e3c
Tim Wanders <timwanders241@gmail.com>
2017-06-11 20:45:43 +0200

    Added main script

--- install.sh
+++ install.sh
@@ -1,11 +1,53 @@
 #!/bin/sh
-SCRIPT_DIR=`dirname "$0"`
-CONF_DIR=$SCRIPT_DIR/files/configs
-WALL_DIR=$SCRIPT_DIR/files/wallpaper
-mkdir ~/Pictures/wallpaper/
-cp $WALL_DIR/*  ~/Pictures/wallpaper/
-cd $CONF_DIR
-for dir in * ; do
-	rm -rf ~/.config/$dir
-	cp -r $dir ~/.config/
-done
+install_wall () {
+	if [ ! -d $HOME/Pictures ]; then
+		mkdir $HOME/Pictures
+	fi
+	cp -r files/wallpaper $HOME/Pictures/
+}
+install_conf () {
+	cd files/configs/
+	for dir in * ; do
+		rm -rf ~/.config/$dir
+		cp -r $dir ~/.config/
+	done
+	cd ../../
+}
+install_bash_it () {
+	git clone --depth=1 https://github.com/Bash-it/bash-it.git $HOME/.bash_it
+	sh $HOME/.bash_it/install.sh
+}
+install_dep_yaourt () {
+	yaourt -S --noconfirm `cat files/dep/yaourt`
+}
+install_dep_pacman () {
+	sudo pacman -S --noconfirm `cat files/dep/pacman`
+}
+install_usage () {
+	echo "USAGE: sh install.sh [OPTIONS]"
+	echo "OPTIONS:	"
+	echo "|	--wallpaper"
+	echo "|	--configs"
+	echo "|	--bash_it"
+	echo "|	--all"
+}
+if [ ! $1 ]; then
+install_usage
+fi
+case $1 in
+	"--wallpaper")
+	install_wall
+	;;
+	"--configs")
+	install_conf
+	;;
+	"--bash_it")
+	install_bash_it
+	;;
+	"--all")
+	install_wall
+	install_conf
+	install_bash_it
+	;;
+	*)
+esac
