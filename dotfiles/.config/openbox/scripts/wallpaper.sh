860223b6e7f788166319de32541fc1b15a7078c5
Tim Wanders <timwanders241@gmail.com>
2017-11-18 15:53:52 +0100

    Added wallpaper script, changes bg on time

---
+++ dotfiles/.config/openbox/scripts/wallpaper.sh
@@ -0,0 +1,17 @@
+# Change wallpaper on time 
+# If the time is 8 PM and up then change to the dark wallpaper
+# if the time is 6 AM and up then change to the light wallpaper
+while true; do
+	T=`date +%H`
+	if [ "x$T" != "x$OLDT" ]; then
+		if [ "$T" -gt "20" ]; then
+			echo dark time!
+			feh --bg-fill ~/Pictures/wallpaper/wallpaper_dark.jpg
+		elif [ "$T" -gt "6" ]; then
+			echo light time!
+			feh --bg-fill ~/Pictures/wallpaper/wallpaper_light.png
+		fi
+		OLDT=$T
+	fi
+	sleep 300
+done
