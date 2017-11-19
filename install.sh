16fd29738f29fc61aa765397b23d08d5e188ee1b
Tim Wanders <timwanders241@gmail.com>
2017-11-19 19:52:39 +0100

    Added prebuilt theme package

--- install.sh
+++ install.sh
@@ -2,10 +2,13 @@ CORE_DEPENDENCIES="xorg-server xorg-xinit xorg-xrandr openbox tint2"
 DEPENDENCIES="rxvt-unicode feh gnome-alsamixer rofi nemo
 		obconf lxappearance
 		xfce4-screenshooter"
-THEME_DEPENDENCIES="kvantum-qt5 numix-gtk-theme adapta-gtk-theme 
+THEME_DEPENDENCIES="numix-gtk-theme adapta-gtk-theme 
 	compton	gtk-theme-switch2"
 if `which pacman > /dev/null 2>&1`; then
 	sudo pacman --needed --noconfirm -S $CORE_DEPENDENCIES $DEPENDENCIES $THEME_DEPENDENCIES
+	# Installing prebuilt packages
+	echo Installing prebuilt packages!
+	sudo pacman -U prebuilt/*pkg.tar*
 else
 	echo pacman is not installed!	
 fi
