112645109f97cb08683d6307791348aa0d95c72f
Tim Wanders <timwanders241@gmail.com>
2017-11-19 22:53:52 +0100

    Added --needed to pacman command

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@ if `which pacman > /dev/null 2>&1`; then
 	sudo pacman --needed --noconfirm -S $CORE_DEPENDENCIES $DEPENDENCIES $THEME_DEPENDENCIES
 	# Installing prebuilt packages
 	echo Installing prebuilt packages!
-	sudo pacman -U prebuilt/*pkg.tar*
+	sudo pacman --needed -U prebuilt/*pkg.tar*
 else
 	echo pacman is not installed!	
 fi
