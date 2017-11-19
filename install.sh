88ccf88d9ac01eafa52b694152f4ab31616680de
Tim Wanders <timwanders241@gmail.com>
2017-11-19 14:34:03 +0100

    Added compton

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@ CORE_DEPENDENCIES="xorg-server xorg-xinit xorg-xrandr openbox tint2"
 DEPENDENCIES="rxvt-unicode feh gnome-alsamixer rofi nemo
 		obconf lxappearance
 		xfce4-screenshooter"
-THEME_DEPENDENCIES="kvantum-qt5 numix-gtk-theme"
+THEME_DEPENDENCIES="kvantum-qt5 numix-gtk-theme compton"
 if `which pacman > /dev/null 2>&1`; then
 	sudo pacman --needed --noconfirm -S $CORE_DEPENDENCIES $DEPENDENCIES $THEME_DEPENDENCIES
 else
