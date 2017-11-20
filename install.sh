39a349294b3c689a7d4090d5303bb68f8c061510
Tim Wanders <timwanders241@gmail.com>
2017-11-20 15:26:56 +0100

    Added dependencies

--- install.sh
+++ install.sh
@@ -4,8 +4,12 @@ DEPENDENCIES="rxvt-unicode feh gnome-alsamixer rofi nautilus
 		xfce4-screenshooter"
 THEME_DEPENDENCIES="numix-gtk-theme adapta-gtk-theme 
 	compton	gtk-theme-switch2"
+GNOME_DEPENDENCIES="gnome-calculator gucharmap 
+	ark gimp gnome-mplayer"
+EXTRAS="firefox chromium mplayer libreoffice pidgin vlc"
 if `which pacman > /dev/null 2>&1`; then
-	sudo pacman --needed --noconfirm -S $CORE_DEPENDENCIES $DEPENDENCIES $THEME_DEPENDENCIES
+	sudo pacman --needed --noconfirm -S $CORE_DEPENDENCIES $DEPENDENCIES \
+				$THEME_DEPENDENCIES $GNOME_DEPENDENCIES $EXTRAS
 	# Installing prebuilt packages
 	echo Installing prebuilt packages!
 	sudo pacman --needed -U prebuilt/*pkg.tar*
