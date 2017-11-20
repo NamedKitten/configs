5031eea5b9806a52f56be2cce946251eefc8ff86
Tim Wanders <timwanders241@gmail.com>
2017-11-20 15:07:48 +0100

    Replace nemo with nautilus

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 CORE_DEPENDENCIES="xorg-server xorg-xinit xorg-xrandr openbox tint2"
-DEPENDENCIES="rxvt-unicode feh gnome-alsamixer rofi nemo
+DEPENDENCIES="rxvt-unicode feh gnome-alsamixer rofi nautilus
 		obconf lxappearance
 		xfce4-screenshooter"
 THEME_DEPENDENCIES="numix-gtk-theme adapta-gtk-theme 
