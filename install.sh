dc8555b41d64ffe510519a81f1dbf9c5b87bd161
Tim Wanders <timwanders241@gmail.com>
2017-12-02 20:27:13 +0100

    Added DESKTOP_THEME_TOOLS

--- install.sh
+++ install.sh
@@ -3,12 +3,13 @@ DEPENDENCIES="xterm rxvt-unicode feh rofi nautilus
 		obconf lxappearance
 		xfce4-screenshooter arandr"
 THEME_DEPENDENCIES="arc-gtk-theme papirus-icon-theme compton"
+DESKTOP_THEME_TOOLS="obconf lxappearance"
 GNOME_DEPENDENCIES="gnome-calculator pavucontrol gnome-mplayer 
 	gucharmap file-roller gimp gcolor2"
 EXTRAS="firefox chromium mplayer libreoffice pidgin vlc redshift gksu gparted"
 if `which pacman > /dev/null 2>&1`; then
 	sudo pacman --needed --noconfirm -S $CORE_DEPENDENCIES $DEPENDENCIES \
-				$THEME_DEPENDENCIES $GNOME_DEPENDENCIES $EXTRAS
+				$THEME_DEPENDENCIES $DESKTOP_THEME_TOOLS $GNOME_DEPENDENCIES $EXTRAS
 	# Installing prebuilt packages
 	echo Installing prebuilt packages!
 	sudo pacman --needed -U prebuilt/*pkg.tar*
