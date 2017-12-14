65ce8407f9ce9c98fa3563a13fe31247033e19fb
Tim Wanders <timwanders241@gmail.com>
2017-12-14 23:41:43 +0100

    Nautilus dependency update

--- install.sh
+++ install.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 CORE_DEPENDENCIES="xorg-server xorg-xinit xorg-xrandr tint2 git pulseaudio"
-DEPENDENCIES="xterm rxvt-unicode feh rofi nautilus
+DEPENDENCIES="xterm rxvt-unicode feh rofi
 		xfce4-screenshooter arandr"
 THEME_DEPENDENCIES="arc-gtk-theme papirus-icon-theme compton qt5ct qt5-styleplugins"
 DESKTOP_THEME_TOOLS="obconf lxappearance lxappearance-obconf"
 GNOME_DEPENDENCIES="gnome-calculator pavucontrol gnome-mplayer gedit
-	gucharmap file-roller gimp gcolor2"
+	gucharmap file-roller gimp gcolor2
+	nautilus filemanager-actions nautilus-open-terminal"
 EXTRAS="firefox chromium mplayer libreoffice-fresh pidgin qt4 vlc redshift gksu gparted"
 if [ "x$1" != "x--source" ]; then
 	if `which pacman > /dev/null 2>&1`; then
@@ -21,9 +22,11 @@ if [ "x$1" != "x--source" ]; then
 	mkdir -p ~/.config/
 	mkdir -p ~/.themes/
 	mkdir -p ~/Pictures/wallpaper/
+	mkdir -p ~/Templates/
 	cp -R dotfiles/.config/* ~/.config/
 	cp -R dotfiles/.themes/* ~/.themes/
 	cp -R wallpaper/* ~/Pictures/wallpaper/
+	cp -R dotfiles/Templates/* ~/Templates/
 	cp dotfiles/.gtkrc-2.0 ~/.gtkrc-2.0 
 	cp dotfiles/.Xdefaults ~/.Xdefaults
 	echo "exec openbox-session" > ~/.xinitrc
