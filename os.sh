6e9a0dedd0b3e94ed5eb2578bc21530d49d88825
Tim Wanders <timwanders241@gmail.com>
2017-12-28 13:33:58 +0100

    More machine specific stuff

--- os.sh
+++ os.sh
@@ -7,10 +7,15 @@ fi
 echo WARNING: THESE PACKAGES APPLY TO MY SYSTEM ONLY
 echo WARNING: CONTINUE AT YOUR OWN RISK!
 read none
+# Locale stuff
+echo Setting locale and generating locales
+sed -i "s/#en/en/g" /etc/locale.gen
+echo "LANG=en_US.UTF-8" > /etc/locale.conf
+locale-gen
 cp system/etc/pacman.conf /etc/pacman.conf
 DRIVERS="nvidia lib32-nvidia-utils r8168 r8168"
 TOOLS="lshw pkgfile nvidia-settings"
-EXTRAS="mpv mps-youtube youtube-dl"
+EXTRAS="mpv mps-youtube youtube-dl steam steam-native-runtime"
 pacman -Sy --noconfirm $DRIVERS $TOOLS $EXTRAS
 pkgfile --update
 # Blacklist the r8169 driver, to force the use of the r8168 driver
