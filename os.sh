ec753b6f1c56f4716208f9524a0909488154d7d8
Tim Wanders <timwanders241@gmail.com>
2018-01-02 13:37:28 +0100

    Fixed trizen command

--- os.sh
+++ os.sh
@@ -6,23 +6,23 @@ audio () {
 	# Fix audio issue I have
 	echo Fixing audio
 	if [ "x`grep -R "load-module module-udev-detect tsched=0" /etc/pulse/default.pa`" = "x" ]; then
-		sed -i "s/load-module module-udev-detect/load-module module-udev-detect tsched=0/g" /etc/pulse/default.pa
+		sudo sed -i "s/load-module module-udev-detect/load-module module-udev-detect tsched=0/g" /etc/pulse/default.pa
 	fi
 }
 ethernet () {
 	# Blacklist the r8169 driver, to force the use of the r8168 driver
 	echo Blacklisting the r8169 module
- 	echo blacklist r8169 > /etc/modprobe.d/ethernet.conf
+ 	echo blacklist r8169 | sudo tee /etc/modprobe.d/ethernet.conf
 }
 services () {
 	# Enable services
 	echo Enabling services
-	systemctl enable dnsmasq
-	systemctl enable dhcpcd
+	sudo systemctl enable dnsmasq
+	sudo systemctl enable dhcpcd
 }
 # installing stuff that I want on my os install
-if [ $UID != 0 ]; then
-	echo Execute this script as root!
+if [ $UID = 0 ]; then
+	echo Execute this script as a user!
 	exit 1
 fi
 echo WARNING: THESE PACKAGES APPLY TO MY SYSTEM ONLY
@@ -34,11 +34,11 @@ if [ ! -f "install_completed" ]; then
 fi
 #
 # Copy configs
-cp system/etc/*.conf /etc/
+sudo cp system/etc/*.conf /etc/
 #
 # Install required packages and update db
-pacman -Sy --noconfirm $DRIVERS $TOOLS $EXTRAS
-pkgfile --update
+sudo pacman -Sy --noconfirm $DRIVERS $TOOLS $EXTRAS
+sudo pkgfile --update
 trizen -S dotnet-sdk-2.0 --noconfirm
 # 
 #
