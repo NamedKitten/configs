ff52e4d11029b7e49a9477cf804117c67b8bd7d1
Tim Wanders <timwanders241@gmail.com>
2018-01-13 22:58:50 +0100

    Added a more universal autostart

---
+++ dotfiles/.config/tim241/autostart.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+# Set solid background as 'backup'
+xsetroot -solid "#303642" & 
+# Fix for crashing DBUS processes: https://bbs.archlinux.org/viewtopic.php?id=224787
+dbus-update-activation-environment --systemd DBUS_SESSION_BUS_ADDRESS DISPLAY XAUTHORITY &
+# Load Xdefaults
+$HOME/.config/tim241/bin/lxdef &
+# Execute xrandr scripts
+$HOME/.config/tim241/xrandr/*.sh && sh ~/.config/tim241/bin/wallpaper &
+# Start compton
+$HOME/.config/tim241/bin/compton &
+
