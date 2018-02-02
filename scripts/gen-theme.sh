ad8e9d11005b40654a430a6fcff083d160dc2491
Tim Wanders <timwanders241@gmail.com>
2018-02-02 11:05:29 +0100

    [gen-theme.sh] Fixes

--- scripts/gen-theme.sh
+++ scripts/gen-theme.sh
@@ -1,10 +1,15 @@
-#!/bin/sh
+#!/bin/bash
 if [ ! $2 ]; then
 	echo Error: Missing arguments!
-	echo Example: $0 [wallpaper] [theme_name]
+	echo Example: $0 [wallpaper] [theme_name] [transparency]
 	exit 1
 fi
-function execx () {
+if [ ! $3 ]; then
+	TRANS=100
+else
+	TRANS=$3
+fi
+function execx {
 	$@
 	exitcode=$?
 	if [ "$exitcode" != "0" ]; then
@@ -18,13 +23,15 @@ if [ -d "$HOME/.config/tim241/themes/$THEME" ]; then
 	rm -rf $HOME/.config/tim241/themes/$THEME
 fi
 echo Creating directories..
-execx mkdir "$HOME/.config/tim241/themes/$THEME" "$HOME/.config/tim241/themes/$THEME/gtk" "$HOME/.config/tim241/themes/$THEME/gtk/gtk3" "$HOME/.config/tim241/themes/$THEME/wallpaper"
+execx mkdir "$HOME/.config/tim241/themes/$THEME" "$HOME/.config/tim241/themes/$THEME/gtk" "$HOME/.config/tim241/themes/$THEME/gtk/gtk3" \
+		"$HOME/.config/tim241/themes/$THEME/wallpaper" "$HOME/.config/tim241/themes/$THEME/rofi"
 echo Generating color scheme using pywal..
-execx wal -i $WALLPAPER -c
+execx wal -i $WALLPAPER -c -a $TRANS
 echo Copying wallpaper..
 execx cp $WALLPAPER $HOME/.config/tim241/themes/$THEME/wallpaper/
-echo Creating Xdefaults..
+echo Copying files..
 execx cp $HOME/.cache/wal/colors.Xresources $HOME/.config/tim241/themes/$THEME/Xdefaults
-cat $HOME/.cache/wal/colors-rofi.Xresources >> $HOME/.config/tim241/themes/$THEME/Xdefaults
+execx cp $HOME/.cache/wal/colors-rofi-dark.rasi $HOME/.config/tim241/themes/$THEME/rofi/colors-rofi.rasi
+execx cp $HOME/.cache/wal/sequences $HOME/.config/tim241/themes/$THEME/sequences
 echo Done
 
