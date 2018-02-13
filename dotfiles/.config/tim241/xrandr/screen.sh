7b90e92e627cca934b9b3b78bf6177362cd8a8a8
Tim Wanders <timwanders241@gmail.com>
2018-02-13 18:06:32 +0100

    Added windows configs(?)

--- dotfiles/.config/tim241/xrandr/screen.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-xrandr --output DVI-1-0 --off --output DVI-D-0 --off --output HDMI-1-0 --off --output VGA-1-0 --mode 1440x900 --pos 0x0 --rotate normal --output HDMI-0 --mode 1440x900 --pos 1440x0 --rotate normal --output DP-1 --off --output DP-0 --off
--- scripts/gen_random_theme.sh
+++
@@ -1,1 +0,0 @@
-curl https://source.unsplash.com/random/1920x1080 -L > random_wall.png && wal -i random_wall.png -a 90 -g -c || echo "Oh no, it failed!" && exit 1
