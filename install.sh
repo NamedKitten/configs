b73d1782ee550298827afe33ec759552bccea3e2
Tim Wanders <timwanders241@gmail.com>
2018-04-17 17:13:03 +0200

    [scripts] Added manjaro

--- install.sh
+++ install.sh
@@ -2,4 +2,8 @@
 set -e
 trap "echo '==> Command failed to execute!'; exit 1" ERR
 source /etc/os-release
-bash "./scripts/platforms/$ID_LIKE.sh"
+if [ "$ID_LIKE" ]; then
+	bash "./scripts/platforms/$ID_LIKE.sh"
+elif [ "$ID" ]; then
+	bash "./scripts/platforms/$ID.sh"
+fi
